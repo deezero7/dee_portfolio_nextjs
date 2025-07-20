cf085507611cf6f68e324388c949707d298b7e05
Deepndra singh <46988044+deezero7@users.noreply.github.com>
2025-07-20 11:53:36 +0530

    lint disable

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    //disables ESLint checks during builds (on Vercel)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
